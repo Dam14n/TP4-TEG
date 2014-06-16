b752fcdc904c65cf6a8d201b84f34c4f8caa9b20
aguscarlavan <agus.carlavan@gmail.com>
2014-06-15 23:26:56 -0300

    Create JugadorInteligente.py

---
+++ JugadorInteligente.py
@@ -0,0 +1,12 @@
+
+from constantes import *
+from jugador import Jugador
+
+class JugadorInteligente(Jugador):
+	"""
+	Este jugador va a ganar el torneo!
+
+	(Escribir codigo de aca para abajo)
+	"""
+	pass
+
